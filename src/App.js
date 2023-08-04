8887373449cf155e6778cbecae74518ea1034110
GabrielSilvaCosta <bob@>
2023-08-04 11:21:15 -0300

    projeto bandeira atualizado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getAllCountries } from './services/countriesAPI';
 import './CountriesList.css';
-//
+
 
 const CountriesList = () => {
   const [countries, setCountries] = useState([]);
